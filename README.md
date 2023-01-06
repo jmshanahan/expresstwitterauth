8d59e2a45ba07ea2f2a19927b9a62e6f35183805
jmshanahan <comeraghsolutions@gmail.com>
2023-01-07 10:54:40 +1100

    Safe point

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # INTRODUCTION
 This is a user authorisation microservice for express notes twitter application.
+
+
+node cli.mjs --host 10.225.58.223 --port 5858 add --password w0rd --family-name kelly --given-name Moira --email moira@stolen.tardis moira
+
+
+
+10.225.58.223
+
+
+
+DEBUG=users:* PORT=5858 REST_LISTEN=0.0.0.0 SEQUELIZE_CONNECT=sequelize-mysql.yaml /opt/pm2-test/node_modules/.bin/pm2 start ./user-server.mjs
--- user-server.mjs
+++ user-server.mjs
@@ -6,8 +6,8 @@ import {
 } from './users-sequelize.mjs';
 
 import DBG from 'debug';
-const log = DBG('users:service'); 
-const error = DBG('users:error'); 
+const log = DBG('users:service');
+const error = DBG('users:error');
 
 import { default as bcrypt } from 'bcrypt';
 
@@ -25,12 +25,22 @@ server.use(restify.plugins.bodyParser({
     mapParams: true
 }));
 
-server.listen(process.env.PORT, "localhost", function() {
-    log(server.name +' listening at '+ server.url);
-});
+// server.listen(process.env.PORT, "localhost", function() {
+//     log(server.name +' listening at '+ server.url);
+// });
 
-process.on('uncaughtException', function(err) { 
-    console.error("UNCAUGHT EXCEPTION - "+ (err.stack || err));
+
+
+server.listen(process.env.PORT,
+    process.env.REST_LISTEN ? process.env.REST_LISTEN :
+        'localhost',
+    function () {
+        log(`${server.name} listening at ${server.url}`);
+    });
+
+
+process.on('uncaughtException', function (err) {
+    console.error("UNCAUGHT EXCEPTION - " + (err.stack || err));
     process.exit(1);
 });
 
@@ -44,14 +54,14 @@ process.on('unhandledRejection', (reason, p) => {
 // Create a user record
 server.post('/create-user', async (req, res, next) => {
     try {
-        log('create-user params '+ util.inspect(req.params));
+        log('create-user params ' + util.inspect(req.params));
         await connectDB();
         let result = await createUser(req);
-        log('created '+ util.inspect(result));
+        log('created ' + util.inspect(result));
         res.contentType = 'json';
         res.send(result);
         next(false);
-    } catch(err) {
+    } catch (err) {
         res.send(500, err);
         error(`/create-user ${err.stack}`);
         next(false);
@@ -65,13 +75,13 @@ server.post('/update-user/:username', async (req, res, next) => {
         await connectDB();
         let toupdate = userParams(req);
         log(`updating ${util.inspect(toupdate)}`);
-        await SQUser.update(toupdate, { where: { username: req.params.username }});
+        await SQUser.update(toupdate, { where: { username: req.params.username } });
         const result = await findOneUser(req.params.username);
-        log('updated '+ util.inspect(result));
+        log('updated ' + util.inspect(result));
         res.contentType = 'json';
         res.send(result);
         next(false);
-    } catch(err) {
+    } catch (err) {
         res.send(500, err);
         error(`/update-user/${req.params.username} ${err.stack}`);
         next(false);
@@ -80,7 +90,7 @@ server.post('/update-user/:username', async (req, res, next) => {
 
 // Find a user, if not found create one given profile information
 server.post('/find-or-create', async (req, res, next) => {
-    log('find-or-create '+ util.inspect(req.params));
+    log('find-or-create ' + util.inspect(req.params));
     try {
         await connectDB();
         let user = await findOneUser(req.params.username);
@@ -88,11 +98,11 @@ server.post('/find-or-create', async (req, res, next) => {
             user = await createUser(req);
             if (!user) throw new Error('No user created');
         }
-        log('find-or-created '+ util.inspect(user));
+        log('find-or-created ' + util.inspect(user));
         res.contentType = 'json';
         res.send(user);
         return next(false);
-    } catch(err) {
+    } catch (err) {
         res.send(500, err);
         error(`/find-or-create ${err.stack}`);
         next(false);
@@ -105,13 +115,13 @@ server.get('/find/:username', async (req, res, next) => {
         await connectDB();
         const user = await findOneUser(req.params.username);
         if (!user) {
-            res.send(404, new Error("Did not find "+ req.params.username));
+            res.send(404, new Error("Did not find " + req.params.username));
         } else {
             res.contentType = 'json';
             res.send(user);
         }
         next(false);
-    } catch(err) {
+    } catch (err) {
         res.send(500, err);
         error(`/find/${req.params.username} ${err.stack}`);
         next(false);
@@ -131,8 +141,8 @@ server.del('/destroy/:username', async (req, res, next) => {
             res.contentType = 'json';
             res.send({});
         }
-        next(false); 
-    } catch(err) {
+        next(false);
+    } catch (err) {
         res.send(500, err);
         error(`/destroy/${req.params.username} ${err.stack}`);
         next(false);
@@ -148,9 +158,9 @@ server.post('/password-check', async (req, res, next) => {
         log(`userPasswordCheck query=${req.params.username} ${req.params.password} user=${user.username} ${user.password}`);
         let checked;
         if (!user) {
-            checked = { 
-                check: false, username: req.params.username, 
-                message: "Could not find user" 
+            checked = {
+                check: false, username: req.params.username,
+                message: "Could not find user"
             };
         } else {
             let pwcheck = false;
@@ -160,17 +170,17 @@ server.post('/password-check', async (req, res, next) => {
             if (pwcheck) {
                 checked = { check: true, username: user.username };
             } else {
-                checked = { 
+                checked = {
                     check: false, username: req.params.username,
-                    message: "Incorrect username or password" 
+                    message: "Incorrect username or password"
                 };
             }
         }
         log(`passwordCheck result=${util.inspect(checked)}`);
         res.contentType = 'json';
         res.send(checked);
-        next(false); 
-    } catch(err) {
+        next(false);
+    } catch (err) {
         res.send(500, err);
         error(`/passwordCheck ${err.stack}`);
         next(false);
@@ -188,7 +198,7 @@ server.get('/list', async (req, res, next) => {
         res.contentType = 'json';
         res.send(userlist);
         next(false);
-    } catch(err) {
+    } catch (err) {
         res.send(500, err);
         error(`/list ${err.stack}`);
         next(false);
@@ -197,15 +207,15 @@ server.get('/list', async (req, res, next) => {
 
 // Mimic API Key authentication.
 
-var apiKeys = [ { user: 'them', key: 'D4ED43C0-8BD6-4FE2-B358-7C0E230D11EF' } ];
+var apiKeys = [{ user: 'them', key: 'D4ED43C0-8BD6-4FE2-B358-7C0E230D11EF' }];
 
 function check(req, res, next) {
     log(`check ${util.inspect(req.authorization)}`);
     if (req.authorization && req.authorization.basic) {
         var found = false;
         for (let auth of apiKeys) {
-            if (auth.key  === req.authorization.basic.password
-             && auth.user === req.authorization.basic.username) {
+            if (auth.key === req.authorization.basic.password
+                && auth.user === req.authorization.basic.username) {
                 found = true;
                 break;
             }
@@ -213,7 +223,7 @@ function check(req, res, next) {
         if (found) next();
         else {
             res.send(401, new Error("Not authenticated"));
-            error('Failed authentication check '+ util.inspect(req.authorization));
+            error('Failed authentication check ' + util.inspect(req.authorization));
             next(false);
         }
     } else {
